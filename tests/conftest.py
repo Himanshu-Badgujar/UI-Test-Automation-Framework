d63ff0f3de4428dc4d31927d4e0f6b737e62bf83
Himanshu Badgujar <h.spmid@gmail.com>
2025-05-18 16:29:40 +0200

    Added --headless mode

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,6 +14,10 @@ def driver(request):
 
     options = Options()
     options.add_argument("--incognito")
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
 
     service = Service()
 
